83db21cc2b680ccd3dbc126ef0e754cd1e38d26e
carmoreno <carlosandresmorenovelez@gmail.com>
2016-11-07 11:38:20 -0500

    endpoint view finished.

---
+++ controladores/endpointCtrl.js
@@ -0,0 +1,15 @@
+var ecolodApp = angular.module('EcolodApp')
+ecolodApp.controller('endpointCtrl', ['$scope', '$route', 'queryService',
+    function($scope, $route, queryService) {
+    	$scope.ruta = $route
+    	$scope.resultado = {}
+    	$scope.consultarEndpoint = function(query) {
+    		queryService.endpoint(query, $scope.resultado)
+    	}
+
+    	// function getKeys(object) {
+    	// 	console.log(object)
+    	// }
+    }
+
+]);
